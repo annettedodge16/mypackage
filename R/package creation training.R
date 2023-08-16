de0392f71ac8b1a562c4c9aa39b57b79f861be56
Annette Dodge <annettedodge16@gmail.com>
2023-08-16 09:10:30 -0400

    update script before creating github repository

--- R/package creation training.R
+++ R/package creation training.R
@@ -2,6 +2,14 @@ library(usethis)
 library(roxygen2)
 
 #create package and indicate file path with the last item being the name of your new package
+#creates package structure and a skeleton of package files
 usethis::create_package("C:/Users/ADODGE/OneDrive - HC-SC PHAC-ASPC/Documents/DupSeq R Package Building/Training/mypackage")
 
+#creates a basic package documentation
 usethis::use_package_doc()
+
+#create a git repository
+usethis::use_git()
+
+#create a github repository
+usethis::use_github()
