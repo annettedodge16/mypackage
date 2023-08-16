efc33a2955ec684fc9cda2f2ffccde570ab84d0e
Annette Dodge <annettedodge16@gmail.com>
2023-08-16 09:06:57 -0400

    Initial commit

---
+++ R/package creation training.R
@@ -0,0 +1,7 @@
+library(usethis)
+library(roxygen2)
+
+#create package and indicate file path with the last item being the name of your new package
+usethis::create_package("C:/Users/ADODGE/OneDrive - HC-SC PHAC-ASPC/Documents/DupSeq R Package Building/Training/mypackage")
+
+usethis::use_package_doc()
