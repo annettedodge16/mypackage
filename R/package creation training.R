f2abb4c5086c9589ac19ef13f50b60828e0275d9
Annette Dodge <annettedodge16@gmail.com>
2023-08-16 10:12:55 -0400

    ok

--- R/package creation training.R
+++ R/package creation training.R
@@ -1,10 +1,10 @@
-library(usethis)
-library(roxygen2)
-library(devtools)
+#library(usethis)
+#library(roxygen2)
+#library(devtools)
 
 #create package and indicate file path with the last item being the name of your new package
 #creates package structure and a skeleton of package files
-usethis::create_package("C:/Users/ADODGE/OneDrive - HC-SC PHAC-ASPC/Documents/DupSeq R Package Building/Training/mypackage")
+##usethis::create_package("C:/Users/ADODGE/OneDrive - HC-SC PHAC-ASPC/Documents/DupSeq R Package Building/Training/mypackage")
 
 #.Rbuildignore lists files that we need to have around but that should not be included when building the R package from source.
 #.Rproj.user, if you have it, is a directory used internally by RStudio.
@@ -15,23 +15,23 @@ usethis::create_package("C:/Users/ADODGE/OneDrive - HC-SC PHAC-ASPC/Documents/Du
 #mypackage.Rproj is the file that makes this directory an RStudio Project.
 
 #creates a basic package documentation
-usethis::use_package_doc()
+##usethis::use_package_doc()
 #set the license for your package
-usethis::use_mit_license("Annette Dodge")
+##usethis::use_mit_license("Annette Dodge")
 
 #create a git repository
-usethis::use_git()
+##usethis::use_git()
 
 #create a github repository
-usethis::use_github()
+##usethis::use_github()
 
 #register your personal access token with rstudio
-gitcreds::gitcreds_set()
+##gitcreds::gitcreds_set()
 
 #Adding functions
-usethis::use_r("testfx1")
+##usethis::use_r("testfx1")
 #We don't need to run the function, we will load it
-load_all()
+##load_all()
 
 #use roxygen to create the documentation for the new function
-usethis::use_roxygen_md()
+##usethis::use_roxygen_md()
